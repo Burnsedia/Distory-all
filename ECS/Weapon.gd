810f10dcf4ee4edfab3b5a0f7959b09bbf3490ca
Burnsedia <bburnsed1995@gmail.com>
2021-06-18 00:19:35 -0400

    changed name of Area.gd to Weapon.gd

--- ECS/Weapon.gd
+++ ECS/Weapon.gd
@@ -1,10 +1,11 @@
 class_name Weapon
 extends Area
 
-
+# Bullet Prebab
 var Bullet = preload("res://ECS/bullit.tscn")
-
+# Direction that the bullet will be fired
 var dir = transform.basis.z
+# Position of the object in 3d space
 var pos = transform.origin
 
 # Fires a unchared shot, no seconday abbilies
@@ -12,7 +13,6 @@ func shoot():
 	# when shoot event detected, fire the weapon when 
 	Events.emit_signal("shoot", Bullet, dir, pos )
 		
-#fires a charged shot, does
-func charged_shot():
+# Fires a charged shot, does
+func charged_shot(target=null):
 	pass
-	
