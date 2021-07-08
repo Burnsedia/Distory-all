6516f0c611fcdea14c5665f18d891aedb778a9ef
Burnsedia <bburnsed1995@gmail.com>
2021-07-07 21:00:55 -0400

    refactoring the UI

---
+++ UI/StartMenu.gd
@@ -0,0 +1,18 @@
+extends Panel
+
+func _init():
+	print("Startmenu has intered the tree")
+
+func _ready():
+	print("I am at the Login Screen")
+
+func _input(event):
+	if InputEventScreenTouch:
+		print(event.device)
+		
+func _process(_delta):
+	pass
+		
+func _on_TouchScreenButton_pressed():
+	print("I have been clicket")
+	get_tree().change_scene("res://ProxyamStarsystem/GameWorld.tscn")
---
+++ UI/UI.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+
+onready var StatMenu = $Panel
+
+func _init():
+	print("I am intering the tree")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("I am on the Tree")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _unhandled_input(delta):
+	if Input.is_action_pressed("start-game"):
+		StatMenu.visible = false
+		get_tree().change_scene("res://Root.tscn")
+		
