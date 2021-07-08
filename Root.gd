bbd4db4157365783431d39d125ebc2f597f010ce
Burnsedia <bburnsed1995@gmail.com>
2021-07-07 21:02:26 -0400

    working on refactoring the code base

--- Root.gd
+++ Root.gd
@@ -12,4 +12,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	print("I am running")
+	if InputEventScreenTouch:
+		print("I touch the screen")
