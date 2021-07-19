3123bcff316559b2202737f7722694b4b716cbbe
Burnsedia <bburnsed1995@gmail.com>
2021-07-19 09:14:12 -0400

    adding the Tower in the Tower definse

---
+++ NPCs/Stations/MainTower.gd
@@ -0,0 +1,26 @@
+extends StaticBody
+
+
+# Declare member variables here. Examples:
+var isDead:bool = false
+export var hp:int = 500
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Global.MainTower = self
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func take_damage(damage):
+	if hp >= 0:
+		Events.emit_signal("game_over")
+		queue_free()
+		get_tree().paused = true
+	else:
+		hp - damage
+		
