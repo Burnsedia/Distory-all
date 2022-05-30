e28b65b8a4827ca985e00cd22b3a2ba7c71aa010
Burnsedia <bburnsed1995@gmail.com>
2022-05-30 04:28:39 -0400

    Tweaks to Drone.gd

--- NPCs/Ships/Drone.gd
+++ NPCs/Ships/Drone.gd
@@ -36,13 +36,13 @@ func _ready():
 
 # Physics Function
 func _physics_process(delta:float) -> void:
-	if Engine.get_idle_frames()%2==0:
-		if transform.origin.distance_to(target.transform.origin) < free_range:
-			avoid(delta)
-		elif transform.origin.distance_to(target.transform.origin) <= attact_range:
-			attack(delta)
-		else:
-			seek(delta)
+
+	if transform.origin.distance_to(target.transform.origin) < free_range:
+		avoid(delta)
+	elif transform.origin.distance_to(target.transform.origin) <= attact_range:
+		attack(delta)
+	else:
+		seek(delta)
 		
 func seek(delta):
 	var target_location = target.global_transform.origin
