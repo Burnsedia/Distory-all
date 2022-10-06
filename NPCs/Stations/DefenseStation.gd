84881d1789925beddde420109f9a1a92cb452ebb
Burnsedia <bburnsedia@gamil.com>
2022-10-06 13:40:22 -0400

    Starting to add the economy part of my station building

--- NPCs/Stations/DefenseStation.gd
+++ NPCs/Stations/DefenseStation.gd
@@ -1,11 +1,7 @@
 extends Station
 class_name DefenseStation
 var target = null
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var mineralCost = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
