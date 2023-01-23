6c92a490e81a390d1e04e6b7f6e823d1ef2b9a08
Burnsedia <bburnsed1995@gmail.com>
2023-01-23 17:07:08 -0500

    Refacted PlayerCamera.gd to be like my Freelancer Template

--- Player/PlayerCamera.gd
+++ Player/PlayerCamera.gd
@@ -1,19 +1,11 @@
 extends Camera
 
-var target = get_parent().transform.origin
-var pos = transform.origin
-var offset = pos - target
+export (float) var weight = 0.8
 
-export var hieght = 4
-export var distancse = 3
-
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	set_as_toplevel(true)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta):
-	look_at_from_position(pos, target, Vector3.UP)
+func _process(delta):
+  var target = get_parent().global_transform
+  global_transform = global_transform.interpolate_with(target, weight)
+	
